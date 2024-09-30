2eaf6b09e448a1f62ecde5cffc3da6b7280897cf
ebaze-tech <ebaze.technologies@gmail.com>
2024-09-30 20:11:23 +0100

    Fixed admin home page

--- src/Admin/AdminHomepage.jsx
+++ src/Admin/AdminHomepage.jsx
@@ -84,7 +84,7 @@ const AdminHomePage = () => {
               //   onClick={() => navigate("/admin/tests")}
               sx={{
                 // marginRight: "20px",
-                padding: "10px 40px",
+                padding: "10px 60px",
                 fontSize: "1.2rem",
                 fontWeight: "bold",
                 backgroundColor: "blueviolet",
@@ -102,7 +102,7 @@ const AdminHomePage = () => {
               size="large"
               //   onClick={() => navigate("/admin/leaderboard")}
               sx={{
-                padding: "10px 40px",
+                padding: "10px 60px",
                 fontSize: "1.2rem",
                 fontWeight: "bold",
                 backgroundColor: "#f50057",
